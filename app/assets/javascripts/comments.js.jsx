2791c997aae7ab834133ad5553eab3670752acf3
Fabien Loup <info@norydev.com>
2015-06-20 12:01:51 +0300

    react component load

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -120,7 +120,9 @@ var CommentForm = React.createClass({
   }
 });
 
-React.render(
-  <CommentBox url="comments.json" pollInterval={2000} />,
-  document.getElementById('content')
-);
+$(function() {
+  React.renderComponent(
+    <CommentBox url="comments.json" pollInterval={2000} />,
+    document.getElementById('content')
+  );
+})
